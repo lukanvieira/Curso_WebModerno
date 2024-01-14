3bfb4a2b2c56db4308cc76697bf42c197145a5dc
devluka <lukavieiralk@gmail.com>
2024-01-14 11:16:06 -0300

    Adicionado metodo get no server.js e terminado a parte de axios

--- 11_Ajax/server.js
+++ 11_Ajax/server.js
@@ -36,5 +36,16 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    // Formas de receber dados do front end
+    // req.body
+    // req.query
+    // re.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 // app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
