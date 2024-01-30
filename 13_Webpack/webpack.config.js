64f0a811ffa33c8176d624a2939d57a3f89f6e63
devluka <lukavieiralk@gmail.com>
2024-01-30 10:38:21 -0300

    Atualizando projeto com TersePlugin

--- 13_Webpack/webpack.config.js
+++ 13_Webpack/webpack.config.js
@@ -1,16 +1,37 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TersePlugin = require('terser-webpack-plugin')
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+// Webpack não precisa mais tanto dos dois plugins acima
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    // optimization: {
+    //     // Não se usa mais o trecho de código abaixo
+    //     // minimizer: [
+    //     //     new UglifyJsPlugin({
+    //     //         // cash: true,      // Essas duas propriedades foram adicionadas
+    //     //         // parallel: true   // no curso, porém não são necessarias.
+    //     //     }),
+    //     //     new OptimizeCSSAssetsPlugin({})
+    //     // ]
+    // },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "estilo.css"
+        }),
+        new TersePlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6
+            }
         })
     ],
     module: {
