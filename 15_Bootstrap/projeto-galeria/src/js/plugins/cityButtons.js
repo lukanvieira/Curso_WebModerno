658f0b9b059aff006eeac8fbd510a80ec491515e
devluka <lukavieiralk@gmail.com>
2024-06-16 09:36:48 -0300

    Executando plugin

--- 15_Bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
+++ 15_Bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import { onLoadHtmlSuccess } from '../core/includes'
 
 const duration = 300
 
@@ -40,4 +41,6 @@ $.fn.cityButtons = function () {
     return this
 }
 
-$('[wm-city-buttons]').cityButtons()
+onLoadHtmlSuccess(function () {
+    $('[wm-city-buttons]').cityButtons()
+})
