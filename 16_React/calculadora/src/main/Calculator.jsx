b368783f8f89a8575354d0e3ce975aec40bd2f46
devluka <lukavieiralk@gmail.com>
2024-07-21 18:14:44 -0300

    Implementando a lógica 02

--- 16_React/calculadora/src/main/Calculator.jsx
+++ 16_React/calculadora/src/main/Calculator.jsx
@@ -25,17 +25,39 @@ export default class Caulculator extends Component {
     }
 
     setOperation(operation) {
-        
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+                if (isNaN(values[0]) || !isFinite(values[0])) return
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit(n) {
         if (n === '.' && this.state.displayValue.includes('.')) return
 
-        const clearDisplay = this.state.displayValue === '0' 
+        const clearDisplay = this.state.displayValue === '0'
             || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
-        
+
         this.setState({ displayValue, clearDisplay: false })
 
         if (n !== '.') {
