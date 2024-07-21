946250ffd4a92f302e122f74c2ee3671da0f2708
devluka <lukavieiralk@gmail.com>
2024-07-21 12:11:09 -0300

    Implementando a Lógica 01

--- 16_React/calculadora/src/main/Calculator.jsx
+++ 16_React/calculadora/src/main/Calculator.jsx
@@ -3,7 +3,16 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
 export default class Caulculator extends Component {
+    state = { ...initialState }
+
     constructor(props) {
         super(props)
         this.clearMemory = this.clearMemory.bind(this)
@@ -12,21 +21,37 @@ export default class Caulculator extends Component {
     }
 
     clearMemory() {
-        console.log('limpar')
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
-        console.log(operation)
+        
     }
 
     addDigit(n) {
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')) return
+
+        const clearDisplay = this.state.displayValue === '0' 
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
+        }
     }
 
     render() {
         return (
             <div className="calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.setOperation} operation />
                 <Button label="7" click={this.addDigit} />
