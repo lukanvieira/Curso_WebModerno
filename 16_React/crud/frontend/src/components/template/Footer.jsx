573e356c20814665efa0c4bed0b55aee9806c970
devluka <lukavieiralk@gmail.com>
2024-08-10 19:48:40 -0300

    Implementando Footer

--- 16_React/crud/frontend/src/components/template/Footer.jsx
+++ 16_React/crud/frontend/src/components/template/Footer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import './Footer';
+import './Footer.css';
 
 const Footer = props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Cod<span className="text-danger">3</span>r</strong>
+        </span>
     </footer> 
 
 export default Footer 
