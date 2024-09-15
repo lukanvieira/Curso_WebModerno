a63ef928018b4d4e0281d481708bc23b6be32e0a
devluka <lukavieiralk@gmail.com>
2024-09-15 09:01:40 -0300

    Navegação entre componentes

--- 17_NextJS/fundamentos/src/app/estiloso/page.jsx
+++ 17_NextJS/fundamentos/src/app/estiloso/page.jsx
@@ -1,8 +1,12 @@
 import style from '../styles/Estiloso.module.css'
+import Link from 'next/link';
 
 export default function Estiloso() {
      return (
         <div className={style.roxo}>
+            <Link href="/">
+                Voltar
+            </Link>
             <h1>Estilo usando CSS Módulos</h1>
         </div>
      )
