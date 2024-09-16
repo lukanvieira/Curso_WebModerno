b3d6257e3a8a8ff739f528b11d1916649224f583
devluka <lukavieiralk@gmail.com>
2024-09-16 09:33:26 -0300

    Componente Navegador

--- 17_NextJS/fundamentos/src/app/jsx/page.jsx
+++ 17_NextJS/fundamentos/src/app/jsx/page.jsx
@@ -1,3 +1,5 @@
+import Layout from "@/components/Layout"
+
 export default function Jsx() {
     const titulo = <h1>JSX é um conceito Central</h1>
 
@@ -6,10 +8,12 @@ export default function Jsx() {
     }
 
     return (
-        <div>
-            {titulo}
-            {subtitulo()}
-            {JSON.stringify({nome: 'João'})}
-        </div>
+        <Layout titulo="Entendendo o JSX">
+            <div>
+                {titulo}
+                {subtitulo()}
+                {JSON.stringify({ nome: 'João' })}
+            </div>
+        </Layout>
     )
 }
--- 17_NextJS/fundamentos/src/app/page.jsx
+++ 17_NextJS/fundamentos/src/app/page.jsx
@@ -1,11 +1,19 @@
+import Navegador from "@/components/Navegador";
+import { wrap } from "lodash";
 import Link from "next/link";
 
 export default function Inicio() {
   return (
-    <div>
-      <Link href="/estiloso">
-        Estiloso
-      </Link>
+    <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexWrap: "wrap"
+    }}>
+      <Navegador texto="Estiloso" destino="/estiloso" />
+      <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3" />
+      <Navegador texto="JSX" destino="/jsx" cor="crimson" />
     </div>
   )
 }
