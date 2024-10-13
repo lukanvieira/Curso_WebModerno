0e093d00090824139aad512873215a0dc3eddfbc
devluka <lukavieiralk@gmail.com>
2024-10-13 10:26:56 -0300

    Criado componente Tabela

--- 17_NextJS/next-crud/src/pages/index.tsx
+++ 17_NextJS/next-crud/src/pages/index.tsx
@@ -1,6 +1,16 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4')
+  ]
+
   return (
     <div className={`
     flex justify-center items-center h-screen
@@ -8,7 +18,7 @@ export default function Home() {
     text-white`
     }>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
